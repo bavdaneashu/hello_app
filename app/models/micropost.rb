f87b6a6d644013b4c001bf900d914bf9a63f0d75
ashwinibavdane <ashwini.bavdane@mirraw.com>
2022-06-17 07:04:38 +0530

    save

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,9 @@
 class Micropost < ApplicationRecord
+    class Micropost < ActiveRecord::Base
+        validates :content, length: { maximum: 140 }
+      end
+      class Micropost < ActiveRecord::Base
+        belongs_to :user
+        validates :content, length: { maximum: 140 }
+      end
 end
